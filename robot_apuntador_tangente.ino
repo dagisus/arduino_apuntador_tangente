e8785c4348f7d385b3c3b4d6f11f8db2d253f4b7
Jesus Reyes <jesusreyes.personal@gmail.com>
2015-09-10 15:29:14 -0500

    Update robot_apuntador_tangente.ino
    se cambio la libreria a customstepper.h, no hay pausas en el proceso.

--- robot_apuntador_tangente.ino
+++ robot_apuntador_tangente.ino
@@ -1,72 +1,95 @@
-#include <Stepper.h>
+#include <CustomStepper.h>
 #include <math.h>
 //definir motor pasos por revolucion y pines
-Stepper motor(100, 8, 10, 9, 11);
-double R, Yb,Xc;
+#define trisen1 2
+#define echosen1 3
+#define trisen2 4
+#define echosen2 5
+char tecla;
+double R, Yb,Xc,calibraX,calibraY;
 int grados;
 int pasos;
-const int sensor1 = 6;
-const int sensor2 = 7;
+CustomStepper stepper(8, 9, 10, 11);
+int rotatedeg = 1;
 void setup() {
   Serial.begin(9600);
+  pinMode(trisen1, OUTPUT);
+  pinMode(echosen1, INPUT);
+  pinMode(trisen2, OUTPUT);
+  pinMode(echosen2, INPUT);
+  calibraX = 0;
+  calibraY = 0;
+  stepper.setRPM(12);
+  stepper.setSPR(4075.7728395);
+  calculo();
 }
 
 void loop() {
-  
-  // ejecutando calculo 
-  Serial.println(grados);
-  Serial.println(pasos);
-  calculo();
+
   mover();
-  
 }
-
 void mover(){
-  int total = (grados *4096)/360;
-  int retorno =total*(-1);
-  pasos = total;
-  motor.setSpeed(100);
-  motor.step(total);
-  delay(1000);
-  motor.step(retorno);
-  
-}
 
+  if (stepper.isDone() && rotatedeg == 1)
+  {
+    stepper.setDirection(CCW);
+    stepper.rotateDegrees(grados);
+    rotatedeg = 2;
+  }
+  stepper.run();
+    if (stepper.isDone() && rotatedeg == 2)
+  {
+    stepper.setDirection(CW);
+    stepper.rotateDegrees(grados);
+    rotatedeg = 3;
+  }
+  stepper.run();
+}
 
 double calculo(){
   //Calibrando sensores
-  pinMode(sensor1, OUTPUT);
-  digitalWrite(sensor1, LOW);
+  digitalWrite(trisen1, LOW);
   delayMicroseconds(2);
-  digitalWrite(sensor1, HIGH);
-  delayMicroseconds(5);
-  digitalWrite(sensor1, LOW);
-  pinMode(sensor2, OUTPUT);
-  digitalWrite(sensor2, LOW);
+  digitalWrite(trisen1, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trisen1, LOW);
+  double duration1 = pulseIn(echosen1, HIGH);
+
+  digitalWrite(trisen2, LOW);
   delayMicroseconds(2);
-  digitalWrite(sensor2, HIGH);
-  delayMicroseconds(5);
-  digitalWrite(sensor2, LOW);
-
-  // detectando distancias en milisegundos
-
-  pinMode(sensor1, INPUT);
-  double duration1 = pulseIn(sensor1, HIGH);
-  pinMode(sensor2, INPUT);
-  double duration2 = pulseIn(sensor2, HIGH);
-
-  // Convirtiendo a centimetros
+  digitalWrite(trisen2, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trisen2, LOW);
+  double duration2 = pulseIn(echosen2, HIGH);
+  
+//  // Convirtiendo a centimetros
   double cm1 = duration1  / 29 / 2;
+  Serial.println("distancia en Y");
+  Serial.println(cm1);
   double cm2 = duration2  / 29 / 2;
-  
+  Serial.println("distancia en X");
+  Serial.println(cm2);
   //Calculando Grados usando Formula
-  
+  R = 2;
+  Yb = cm1 + calibraY;
+  Xc = cm2 + calibraX;
   double a = -2*(R*Yb);
+  //Serial.println("a");
+  //Serial.println(a);
   double b = sqrt((4*(pow(R,2)*pow(Yb,2)))+((4*(pow(Yb,2)+pow(Xc,2)))*(pow(Xc,2)-pow(R,2))));
+  //Serial.println("b");
+  //Serial.println(b);
   double c = 2*(pow(Yb,2)+pow(Xc,2));
+  //Serial.println("c");
+  //Serial.println(c);
   double resultado = (a+b)/c;
-  Serial.begin(9600);
-  Serial.print(resultado);
+  //Serial.println("resultado 1");
+  //Serial.println(resultado);
+  resultado = degrees(acos(resultado));
+  //Serial.println("resultado 2");
+  //Serial.println(resultado);
   resultado = resultado+0.5;
   grados = (int)resultado;
+  Serial.println("grados");
+  Serial.println(grados);
 }
